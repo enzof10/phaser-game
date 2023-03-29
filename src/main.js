0491b0424fbf485f4122009878740c27829a6732
enzof10 <enzo_17_05@hotmail.com>
2023-03-29 20:57:01 -0300

    add on key up

--- src/main.js
+++ src/main.js
@@ -45,26 +45,26 @@ function create() {
 
     this.anims.create({
         key: 'left',
-        frame: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 15,
         repeat: -1
     })
 
     this.anims.create({
         key: 'turn',
-        frame: [{ key: 'dude', frame: 4 }],
-        frameRate: 20,
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 15,
     })
 
 
     this.anims.create({
-        key: 'left',
-        frame: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 15,
         repeat: -1
     })
 
-    player.body.setGravityY(300)
+    // player.body.setGravityY(300)
 
     this.physics.add.collider(platform, player)
     
@@ -72,9 +72,21 @@ function create() {
     cursors = this.input.keyboard.createCursorKeys()
 }
 function update() {
+
     if(cursors.left.isDown){
-        console.log('cursors.left: ', cursors.left);
         player.setVelocityX(-160)
+        player.anims.play('left', true)
+    }else if(cursors.right.isDown){
+        player.setVelocityX(160)
+        player.anims.play('right', true)
+    }else{
+        player.setVelocityX(0)
+        player.anims.play('turn', true)
     }
+
+    if(cursors.up.isDown && player.body.touching.down){
+        player.setVelocityY(-330)
+    }
+
 }
 
